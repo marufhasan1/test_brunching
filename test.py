acfbc71bdb26167451469dd9ebe8f0633b8820ce
MD Nazmul Hasan Sheum <mohammadsheam@gmail.com>
2017-11-28 11:18:04 +0600

    Add a python script

---
+++ test.py
@@ -0,0 +1,8 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.sort()
+print (motorcycles)
+motorcycles.sort(reverse=True)
+print(motorcycles)
+motorcycles.reverse()
+print(motorcycles)
+#print("The last motorcycle I owned was a " + last_owned.capitalize()+ ".")
