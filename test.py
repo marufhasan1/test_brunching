6c8432793450a38aec02eb3bd0883cc721050711
MD Nazmul Hasan Sheum <mohammadsheam@gmail.com>
2017-11-28 11:21:37 +0600

    Update code..add function

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-motorcycles = ['honda', 'yamaha', 'suzuki']
-motorcycles.sort()
-print (motorcycles)
-motorcycles.sort(reverse=True)
-print(motorcycles)
-motorcycles.reverse()
-print(motorcycles)
-#print("The last motorcycle I owned was a " + last_owned.capitalize()+ ".")
+def main():
+    motorcycles = ['honda', 'yamaha', 'suzuki']
+    motorcycles.sort()
+    print (motorcycles)
+    motorcycles.sort(reverse=True)
+    print(motorcycles)
+    motorcycles.reverse()
+    print(motorcycles)
+if __name__=="__main__":
+    main()
